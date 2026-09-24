05528c5412395bd484da8c7914ffbf6852a30c0e
Thato Makhusha <makhushathato2@outlook.com>
2026-09-24 13:25:51 +0200

    Changed sitemap url

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://tscakes.co.za",
+            url: "https://www.tscakes.co.za",
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 1,
         },
         {
-            url: "https://tscakes.co.za/order",
+            url: "https://www.tscakes.co.za/order",
             lastModified: new Date(),
             changeFrequency: "weekly",
             priority: 0.8,
